d97175b1dedb708d2087f2d3807316bed2bd7700
1334 <1334@shadowproject.net>
2018-11-09 17:51:48 +0100

    searches are persisted on route changes

--- client/src/app/components/search/search.component.ts
+++ client/src/app/components/search/search.component.ts
@@ -3,6 +3,7 @@ import { fromEvent } from 'rxjs';
 import { debounceTime, filter } from 'rxjs/operators';
 import { ApiClientService } from '../../services/api-client.service';
 import SearchResult from '../../models/search-result';
+import { SearchResultsService } from 'src/app/services/search-results.service';
 
 @Component({
   selector: 'app-search',
@@ -10,10 +11,16 @@ import SearchResult from '../../models/search-result';
   styleUrls: ['./search.component.scss']
 })
 export class SearchComponent implements OnInit {
-  searchText = '';
-  results: SearchResult[] = [];
+  searchText;
+  results: SearchResult[];
 
-  constructor(private apiClient: ApiClientService) {}
+  constructor(
+    private apiClient: ApiClientService,
+    private resultsService: SearchResultsService
+  ) {
+    this.searchText = resultsService.searchText;
+    this.results = resultsService.results;
+  }
 
   ngOnInit() {
     let inputElm = document.getElementById('searchTextField');
@@ -23,9 +30,11 @@ export class SearchComponent implements OnInit {
         filter(() => this.searchText.length > 2)
       )
       .subscribe(() => {
-        this.apiClient
-          .searchTerm(this.searchText)
-          .subscribe(results => (this.results = results));
+        this.apiClient.searchTerm(this.searchText).subscribe(results => {
+          this.results = results;
+          this.resultsService.results = results;
+          this.resultsService.searchText = this.searchText;
+        });
       });
   }
 }
--- client/src/app/services/search-results.service.ts
+++ client/src/app/services/search-results.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
+import SearchResult from '../models/search-result';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SearchResultsService {
+  searchText = '';
+  results: SearchResult[] = [];
 
-  constructor() { }
+  constructor() {}
 }
