3f7cc89b43ed66675c1d0d782601aa0194f3fae0
1334 <1334@shadowproject.net>
2018-11-09 16:24:16 +0100

    refactor shows passthrough cache to grab all the data only when needed

--- server/controllers/showsController.js
+++ server/controllers/showsController.js
@@ -10,38 +10,10 @@ showsController.recommended = async (_, res) => {
 };
 
 showsController.get = async (req, res) => {
-  const show = await getShow(req.params.showId);
-  if (!show.similar.length) {
-    const key = process.env.API_KEY;
-    const id = req.params.showId;
-    await fetch(
-      `https://api.themoviedb.org/3/tv/${id}?api_key=${key}&append_to_response=similar`
-    )
-      .then(data => data.json())
-      .then(async data => {
-        show.number_of_seasons = data.number_of_seasons;
-        show.similar = data.similar.results.map(el => el.id);
-        await data.similar.results.map(async data => {
-          await db.Show.findOrCreate({
-            where: {
-              tmdbId: data.id
-            },
-            defaults: {
-              name: data.name,
-              backdrop_path: data.backdrop_path,
-              number_of_seasons: data.number_of_seasons,
-              vote_average: data.vote_average,
-              overview: data.overview
-            }
-          });
-        });
-        return show.save();
-      });
-  }
-
+  const id = +req.params.showId;
+  const show = await getShow(id);
   const similar = await db.Show.findAll({ where: { tmdbId: show.similar } });
   show.similar = similar;
-
   res.status(200).send(show);
 };
 
@@ -60,25 +32,79 @@ showsController.markToSee = async (req, res) => {
 };
 
 async function getShow(id) {
-  const localShow = await db.Show.findOne({ where: { tmdbId: +id } });
-  if (localShow) return localShow;
+  const localShow = await db.Show.findOne({ where: { tmdbId: id } });
+  if (!localShow) return await createShow(id);
 
+  if (completeInfo(localShow)) return localShow;
+  else return await updateShowInfo(id);
+}
+
+function completeInfo(show) {
+  return !!show.number_of_seasons && !!show.similar.length && !!show.tmdbBlob;
+}
+
+async function createShow(id) {
   const key = process.env.API_KEY;
   return await fetch(
     `https://api.themoviedb.org/3/tv/${id}?api_key=${key}&append_to_response=similar`
   )
     .then(data => data.json())
     .then(async data => {
-      await db.Show.create({
+      const attrs = {
         tmdbId: data.id,
         name: data.name,
         backdrop_path: data.backdrop_path,
+        poster_path: data.poster_path,
         number_of_seasons: data.number_of_seasons,
         vote_average: data.vote_average,
-        overview: data.overview
-      });
-      return data;
+        overview: data.overview,
+        similar: data.similar.results.map(el => el.id),
+        genre_ids: data.genre_ids,
+        tmdbBlob: data
+      };
+      const show = await db.Show.create(attrs);
+      await createSimilarShows(data.similar.results);
+      return show;
     });
+}
+
+async function updateShowInfo(id) {
+  const show = await db.Show.findOne({ where: { tmdbId: id } });
+  const key = process.env.API_KEY;
+
+  return await fetch(
+    `https://api.themoviedb.org/3/tv/${id}?api_key=${key}&append_to_response=similar`
+  )
+    .then(data => data.json())
+    .then(async data => {
+      const attrs = {
+        number_of_seasons: data.number_of_seasons,
+        similar: data.similar.results.map(el => el.id),
+        tmdbBlob: data
+      };
+      await show.update(attrs, { where: { tmdbId: data.id } });
+      await createSimilarShows(data.similar.results);
+      return show;
+    });
+}
+
+async function createSimilarShows(showsArr) {
+  await Promise.all(
+    showsArr.map(async show => {
+      const ss = await db.Show.findOne({ where: { tmdbId: show.id } });
+      if (!ss) {
+        await db.Show.create({
+          tmdbId: show.id,
+          name: show.name,
+          backdrop_path: show.backdrop_path,
+          poster_path: show.poster_path,
+          vote_average: show.vote_average,
+          overview: show.overview,
+          genre_ids: show.genre_ids
+        });
+      }
+    })
+  );
 }
 
 module.exports = showsController;
--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const app = require(__dirname + '/server.js');
 const port = 4000;
 
+// eslint-disable-next-line no-console
 app.listen(port, () => console.log(`Server running on port ${port}!`));
