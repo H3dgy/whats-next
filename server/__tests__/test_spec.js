d14bdf0af7342c2b92d77e2a515432e2facb8c67
1334 <1334@shadowproject.net>
2018-11-09 14:54:58 +0100

    finish tests for the /recommended endpoint

--- server/__tests__/test_spec.js
+++ server/__tests__/test_spec.js
@@ -3,9 +3,20 @@ const app = require('../server');
 
 describe('testing the shows controller', () => {
   describe('GET /recommended', () => {
+    let response;
+    beforeEach(async () => {
+      response = await request(app).get('/recommended');
+    });
     it('returns 20 elements at most', async () => {
-      const response = await request(app).get('/recommended');
       expect(response.body.length).toBeLessThanOrEqual(20);
     });
+    it('returns the needed attrs', async () => {
+      const el = response.body[0];
+      expect(el.tmdbId).not.toBeUndefined();
+      expect(el.name).not.toBeUndefined();
+      expect(el.backdrop_path).not.toBeUndefined();
+    });
   });
+
+  describe('GET /shows/:showId', () => {});
 });
