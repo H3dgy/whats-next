8689ef40970aa10b89e5ef5f9992d736ef088927
Frederik Hermans <frederik.hermans@hotmail.com>
2018-11-19 11:51:07 +0100

    test - refactoring

--- server/config/test_helpers.js
+++ server/config/test_helpers.js
@@ -1,9 +1,6 @@
-const request = require('supertest');
-const app = require('../server');
 const db = require('../models');
 const trackingSeeder = require('../testSeeding/trackingSeeders');
 const trackingController = require('../controllers/trackingController');
-
 
 
 describe('testing the tracking Controller: create status', () => {
