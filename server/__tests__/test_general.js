b40f59cbb6e73e1fda8d494567df601f7a8383b9
Frederik Hermans <frederik.hermans@hotmail.com>
2018-11-19 12:01:24 +0100

    refa: refactored tests

--- server/__tests__/test_general.js
+++ server/__tests__/test_general.js
@@ -2,7 +2,8 @@ const request = require('supertest');
 const app = require('../server');
 const db = require('../models');
 const trackingSeeder = require('../testSeeding/trackingSeeders');
-const userSeeder = require('../testSeeding/userSeeders');
+const trackingController = require('../controllers/trackingController');
+
 
 /**
  * In order to test the creation of the user the database needs to be reset
@@ -302,7 +303,7 @@ describe('testing the tracking Controller: create review', () => {
     await trackingSeeder.downUsers(db.sequelize.queryInterface);
     await trackingSeeder.downShows(db.sequelize.queryInterface);
   });
-  
+
   it('respond with the movie object which includes the tracking', async () => {
     const response = await request(app)
       .post('/user/100/review')
@@ -337,3 +338,131 @@ describe('testing the tracking Controller: create review', () => {
     expect(response.status).toEqual(400);
   });
 });
+
+/**
+ * Test: Check if the interaction is correct and tracking is being called when necessary
+ * Test: Check if the right item gets the review/ status and or rating
+ * Test: Check the output
+ * Test: Check for incorrect input
+ */
+
+describe('testing the tracking Controller: create rating', () => {
+  beforeAll(async () => {
+    await trackingSeeder.downTracking(db.sequelize.queryInterface);
+    await trackingSeeder.downUsers(db.sequelize.queryInterface);
+    await trackingSeeder.downShows(db.sequelize.queryInterface);
+  });
+
+  beforeEach( async () => {
+    await trackingSeeder.upShows(db.sequelize.queryInterface);
+    await trackingSeeder.upUsers(db.sequelize.queryInterface);
+  })
+
+  afterEach( async () => {
+    await trackingSeeder.downTracking(db.sequelize.queryInterface);
+    await trackingSeeder.downShows(db.sequelize.queryInterface);
+    await trackingSeeder.downUsers(db.sequelize.queryInterface);
+  })
+
+  afterAll(async () => {
+    await trackingSeeder.downTracking(db.sequelize.queryInterface);
+    await trackingSeeder.downUsers(db.sequelize.queryInterface);
+    await trackingSeeder.downShows(db.sequelize.queryInterface);
+  });
+
+  it('respond with the movie object which includes the tracking', async () => {
+    await request(app)
+    .post('/user/100/rate')
+    .send({
+      review: 'Great',
+      userId: 1
+    })
+    .set('Content-Type', 'application/json');
+    
+    const response = await request(app)
+      .post('/user/100/rate')
+      .send({
+        rating: 5,
+        userId: 1
+      })
+    .set('Content-Type', 'application/json');
+    expect(response.status).toEqual(201);
+    expect(response.body).toMatchObject({
+      name: "I Am Not an Animal",
+      tracking: {rating: 5, review: null, status: 'none'}
+    });
+  });
+  it('respond with status 400 if given incorrect userId', async () => {
+    const response = await request(app)
+      .post('/user/100/rate')
+      .send({
+        rating: 10,
+        userId: 200
+      })
+      .set('Content-Type', 'application/json');
+    expect(response.status).toEqual(400);
+  });
+  it('respond with status 400 if given incorrect movieId', async () => {
+    const response = await request(app)
+      .post('/user/20003030303/rate')
+      .send({
+        rating: 10,
+        userId: 1
+      })
+    expect(response.status).toEqual(400);
+  });
+  it('respond with status 400 if rating is NaN', async () => {
+    const response = await request(app)
+      .post('/user/20003030303/rate')
+      .send({
+        rating: 'seven',
+        userId: 1
+      })
+    expect(response.status).toEqual(400);
+  });
+  it('respond with status 400 if rating is undefined', async () => {
+    const response = await request(app)
+      .post('/user/20003030303/rate')
+      .send({
+        rating: 10,
+        userId: 1
+      })
+    expect(response.status).toEqual(400);
+  });
+  it('respond with status 400 if rating is out of bounds', async () => {
+    const response = await request(app)
+      .post('/user/20003030303/rate')
+      .send({
+        rating: 100,
+        userId: 1
+      })
+    expect(response.status).toEqual(400);
+  });
+});
+
+/**
+ * Test: helpers
+ */
+
+describe('testing the tracking Controller: create status', () => {
+  beforeAll (async () => {
+    await trackingSeeder.downShows(db.sequelize.queryInterface);
+    await trackingSeeder.downUsers(db.sequelize.queryInterface);
+  })
+  beforeEach(async () => {
+   await trackingSeeder.upShows(db.sequelize.queryInterface);
+   await trackingSeeder.upUsers(db.sequelize.queryInterface);
+  })
+  afterEach(async () => {
+   await trackingSeeder.downShows(db.sequelize.queryInterface);
+   await trackingSeeder.downUsers(db.sequelize.queryInterface);
+  })
+  it('should response with an object with all movies unwrapped', async () => {
+    const res = await trackingController._unwrapSimilar(100,1);
+    expect(res).toMatchObject({
+      similar: [{id: 125}, {id: 135}]
+    });
+  });
+});
+
+
